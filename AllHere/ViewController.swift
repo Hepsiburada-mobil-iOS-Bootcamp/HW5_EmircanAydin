0c0064f9261269bd9b79ad2c44183219dd66a66e
emircanaydin <emircanaydin@menulux.com>
2021-10-20 17:52:44 +0300

    Initial Commit

---
+++ AllHere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllHere
+//
+//  Created by Emircan Aydın on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
